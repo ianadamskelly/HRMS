11e1634b4fdcc2335820936a79e5b859d20287b3
Ian Kelly <ianadamskely@gmail.com>
2025-09-27 12:18:53 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/api

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -7,11 +7,19 @@ import {
   GoogleAuthProvider,
   signInWithPopup,
   signOut,
-  connectAuthEmulator,
-  Auth,
 } from 'firebase/auth';
-import { getApp, getApps, initializeApp } from 'firebase/app';
-import { firebaseConfig } from './config';
+import { getApp, getApps, initializeApp, type FirebaseOptions } from 'firebase/app';
+
+// Manually define the configuration here to ensure it's correct.
+export const firebaseConfig: FirebaseOptions = {
+  "apiKey": "AIzaSyD_ZEhD-4avFJc1rA4BpXhEuDvb2rAW1NU",
+  "authDomain": "hrms-af28f.firebaseapp.com",
+  "projectId": "hrms-af28f",
+  "storageBucket": "hrms-af28f.appspot.com",
+  "messagingSenderId": "924905397621",
+  "appId": "1:924905397621:web:8a4b7496d8e47b26e8873c",
+  "measurementId": "G-FVP0C3XG1H"
+};
 
 // Initialize Firebase app if it hasn't been already
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
